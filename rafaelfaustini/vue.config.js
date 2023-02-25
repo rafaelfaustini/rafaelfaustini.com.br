33b27d4d63f83434b1fdc76b337de1e55748839c
Rafael Faustini <contato@rafaelfaustini.com.br>
2023-02-25 12:21:33 +0000

    Base url base on environment and language

--- rafaelfaustini/vue.config.js
+++ rafaelfaustini/vue.config.js
@@ -1,5 +1,21 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
+function getPublicPath(enviroment, language = "en") {
+  switch (enviroment) {
+    case "production":
+      if(language === "br") {
+        return "/home/";
+      }
+      return "/";
+    case "development":
+      return "/dev/";  
+    case "beta":
+      return "/beta/";  
+    default:
+      break;
+  }
+}
+
 const { argv } = require('yargs')
 module.exports = {
   css: {
@@ -12,16 +28,13 @@ module.exports = {
         },
       },
     },
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/'
-      : '/',
       chainWebpack: config => {
         config.plugin('define').tap(options => {
             options[0]['process.env'].VUE_APP_LANG = `"${argv.lang}"`;
             return options;
         })
      },
-     
+     publicPath: getPublicPath(process.env.NODE_ENV, argv.lang),
   }
 
   
